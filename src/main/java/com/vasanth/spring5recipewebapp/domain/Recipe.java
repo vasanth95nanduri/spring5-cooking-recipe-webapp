712c7086da543fb04e328fb24c7618afac45a47a
vasanth95nanduri <36486845+vasanth95nanduri@users.noreply.github.com>
2018-03-08 18:15:31 -0500

    added couple of domains. Ingredient, Notes, Recipe, UnitOfMeasure.

---
+++ src/main/java/com/vasanth/spring5recipewebapp/domain/Recipe.java
@@ -0,0 +1,111 @@
+package com.vasanth.spring5recipewebapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private String prepTime;
+    private String CookTime;
+    private String serving;
+    private String source;
+    private String url;
+    private String directions;
+    //todo add
+    //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(String prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public String getCookTime() {
+        return CookTime;
+    }
+
+    public void setCookTime(String cookTime) {
+        CookTime = cookTime;
+    }
+
+    public String getServing() {
+        return serving;
+    }
+
+    public void setServing(String serving) {
+        this.serving = serving;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+}
