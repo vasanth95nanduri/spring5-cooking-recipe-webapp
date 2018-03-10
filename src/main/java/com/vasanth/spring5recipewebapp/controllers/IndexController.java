f1941411c8076e4e7526a8b51ad68733ec8817c2
vasanth95nanduri <36486845+vasanth95nanduri@users.noreply.github.com>
2018-03-09 20:29:12 -0500

    added data.sql file to initialize some database values. modified category and UOM repositories. Updated IndexController

--- src/main/java/com/vasanth/spring5recipewebapp/controllers/IndexController.java
+++ src/main/java/com/vasanth/spring5recipewebapp/controllers/IndexController.java
@@ -1,14 +1,36 @@
 package com.vasanth.spring5recipewebapp.controllers;
 
+import com.vasanth.spring5recipewebapp.domain.Category;
+import com.vasanth.spring5recipewebapp.domain.UnitOfMeasure;
+import com.vasanth.spring5recipewebapp.repositories.CategoryRepository;
+import com.vasanth.spring5recipewebapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
-        System.out.println("hello World.......555554444..........");
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByUom("Teaspoon");
+
+        System.out.println("cat id is: "+categoryOptional.get().getId());
+        System.out.println("uom id is: "+unitOfMeasure.get().getId());
+
         return "index";
     }
 
--- src/main/java/com/vasanth/spring5recipewebapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/vasanth/spring5recipewebapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package com.vasanth.spring5recipewebapp.repositories;
 import com.vasanth.spring5recipewebapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByUom(String uom);
+
 }
